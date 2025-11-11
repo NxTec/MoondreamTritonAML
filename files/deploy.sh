07807bc1886d205866d154eab1c9ee88d9c6c17f
sarahnxtec <sarah@nxtec-corp.com>
2025-11-11 14:40:04 +0300

    Delete files directory

--- files/deploy.sh
+++
@@ -1,5 +0,0 @@
-set -euo pipefail
-
-
-
-az ml online-deployment create -f deployment.yml 
--- files/reg_model.sh
+++
@@ -1,6 +0,0 @@
-az ml model create \
-  --name moondream_triton \
-  --version 3 \
-  --type triton_model \
-  --path ./model-repo \
-  --description "base"
